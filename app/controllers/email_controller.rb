d7e56ef27eea1d9d277327e830379662b212c36a
Eric <goodness@demo.app>
2012-12-20 22:38:26 -0800

    Fixing bad method name

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -2,8 +2,8 @@ class EmailController < ApplicationController
   def contact
 
     if request.post? 
-     @email = params[:email] 
-     ContactForm.contact_message(@email).devliver
+     @email = params[:email]
+     ContactForm.contact_message(@email).deliver
     end
   end
 end
