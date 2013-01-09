cb51cf936d245ea5c6e81cd960e0b93c4540c38b
Eric <noneya.buisness>
2013-01-09 04:16:52 +0000

    Adding google analytics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Portfolio::Application.configure do
+  #Google analytics
+  GA.tracker = "UA-37528412-1"
+
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
